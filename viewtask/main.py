8ae77905b9ac6a8c8df4f6a8e074c7ed1b9a8f4d
Jason Fennell <jdfennell@gmail.com>
2014-04-24 08:50:53 -0700

    pulled out some of the auth stuff. lets see if we can get a request object going with the simpler cases, then generalize back to auth

--- viewtask/main.py
+++ viewtask/main.py
@@ -1,15 +1,16 @@
 import json
-import hashlib
 import pprint
 import random
 import requests
 import sys
 import yaml
 
-ENDPOINT = 'https://api.rememberthemilk.com/services/rest/'
+from viewtask import auth
+from viewtask import util
+
 
 class RememberAPI(object):
-    def __init__(self, api_key, shared_secret, token=None, endpoint=ENDPOINT, response_format='json'):
+    def __init__(self, api_key, shared_secret, token=None, endpoint=util.ENDPOINT, response_format='json'):
         self.api_key = api_key
         self.shared_secret = shared_secret
         self.token = token
@@ -25,25 +26,12 @@ class RememberAPI(object):
         if self.token:
             params['token'] = token
         params.update(kwargs)
-        api_sig = self._signature(params)
+        api_sig = util.signature(params)
         params['api_sig'] = api_sig
 
         r = requests.get(self.endpoint, params=params)
         response = json.loads(r.text)
 
-    def _signature(self, params_dict):
-        """Calculate the api signature from the parameters to a request"""
-        # Sort params in alphabetical order,
-        # concatenate k,v pairs
-        # concatenate all of those concatenated pairs
-        # then prepend the secret and md5 hash it
-        sorted_concatendated_params = ''.join(
-            sorted(
-                "{k}{v}".format(k=k, v=v)
-                for k, v in params_dict.iteritems()
-            )
-        )
-        return hashlib.md5(shared_secret+sorted_concatendated_params).hexdigest()
 
 # TODO: namedtuple...
 def load_config(path='config.yaml'):
@@ -56,57 +44,6 @@ def load_config(path='config.yaml'):
 
     return config
 
-def get_frob(api_key, shared_secret):
-    params = {
-        'method': 'rtm.auth.getFrob',
-        'api_key': api_key,
-        'format': 'json'
-    }
-    api_sig = calculate_secret(shared_secret, params)
-    params['api_sig'] = api_sig
-    r = requests.get(
-        ENDPOINT,
-        params=params,
-    )
-    response = json.loads(r.text)['rsp']
-
-    if 'frob' not in response or response['stat'] != 'ok':
-        raise ValueError('Got an invalid response from frob:\n{response}'.format(response=r.text))
-    return response['frob']
-
-def show_authorize_url(api_key, shared_secret):
-    """Construct an auth url for a user.
-
-    https://www.rememberthemilk.com/services/api/authentication.rtm
-    """
-    frob = get_frob(api_key, shared_secret)
-    params = {
-        'api_key': api_key,
-        'perms': 'read',
-        'frob': frob,
-    }
-    api_sig = calculate_secret(shared_secret, params)
-    params['api_sig'] = api_sig
-    endpoint = 'http://www.rememberthemilk.com/services/auth/'
-    r = requests.get(endpoint, params=params)
-    print frob, r.url
-
-def get_auth_token(api_key, shared_secret, frob):
-    params = {
-        'api_key': api_key,
-        'frob': frob,
-        'method': 'rtm.auth.getToken',
-        'format': 'json',
-    }
-    api_sig = calculate_secret(shared_secret, params)
-    params['api_sig'] = api_sig
-    r = requests.get(ENDPOINT, params=params)
-    response = json.loads(r.text)['rsp']
-
-    pprint.pprint(response)
-    auth = response['auth']
-    return auth['token']
-
 
 def main(args=None):
     args = args or sys.argv[1:]
@@ -117,20 +54,15 @@ def main(args=None):
 
     if args:
         if args[0] == 'frob':
-            show_authorize_url(api_key, shared_secret)
+            auth.show_authorize_url(api_key, shared_secret)
         elif args[0] == 'token':
-            token = get_auth_token(api_key, shared_secret, conf['frob'])
+            token = auth.get_auth_token(api_key, shared_secret, conf['frob'])
             print 'Token: "{token}"'.format(token=token)
         return
 
     # Now assume frob & token are in conf and valid
 
-#    params = {
-#        'method': 'rtm.lists.getList',
-#        'format': 'json',
-#        'api_key': api_key,
-#        'auth_token': conf['token']
-#    }
+    # params = { 'method': 'rtm.lists.getList', 'format': 'json', 'api_key': api_key, 'auth_token': conf['token'] }
     params = {
         'method': 'rtm.tasks.getList',
         'filter': '(list:work due:today)',
@@ -138,16 +70,16 @@ def main(args=None):
         'api_key': api_key,
         'auth_token': conf['token'],
     }
-    api_sig = calculate_secret(shared_secret, params)
+    api_sig = util.signature(shared_secret, params)
     params['api_sig'] = api_sig
 
-    r = requests.get(ENDPOINT, params=params)
+    r = requests.get(util.ENDPOINT, params=params)
     response = json.loads(r.text)
-#    pprint.pprint(response)
+    # pprint.pprint(response)
 
     # XXX: This will crash if there is an error
     list_items = response.get('rsp', {}).get('tasks', {}).get('list', [])[0].get('taskseries', {})
-#    pprint.pprint(list_items)
+    # pprint.pprint(list_items)
     task = random.choice(list_items)
     pprint.pprint(task)
     present_task(task)
@@ -160,15 +92,6 @@ def present_task(task):
     print '!{priority}: {description}'.format(
         priority=priority,
         description=description)
-
-
-def calculate_secret(shared_secret, params_dict):
-    sorted_concatendated_params = ''.join(
-        sorted(
-            "{k}{v}".format(k=k, v=v)
-            for k, v in params_dict.iteritems()))
-
-    return hashlib.md5(shared_secret+sorted_concatendated_params).hexdigest()
 
 
 if __name__ == '__main__':
